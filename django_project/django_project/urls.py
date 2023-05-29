e3a61d0d0a0ba3ead6e261de21684c516c9eb464
Muhu-Muza <schumannivan@gmail.com>
2023-05-29 15:23:06 +0300

    Restructured files, added url path for the home page and a view for the home page

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from account import views as user_views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/', user_views.index, name='home_page'),
 ]
