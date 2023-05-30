2b60c0d8a207ca409379733e4e1eb5be3cf9f401
Muhu-Muza <schumannivan@gmail.com>
2023-05-30 10:22:59 +0300

    Created url end points for home, base, services, about and contact, created views for them, edited their corresponding files to respond to jinja templating, abstracted away reused code into the base.html file

--- django_project/account/views.py
+++ django_project/account/views.py
@@ -2,6 +2,18 @@ from django.shortcuts import render
 
 # Create your views here.
 
+def base(request):
+    return render(request, 'base.html', {})
 
 def index(request):
     return render(request, 'index.html', {})
+
+def about(request):
+    return render(request, 'about.html', {})
+
+def services(request):
+    return render(request, 'services.html', {})
+
+def contact(request):
+    return render(request, 'contact.html', {})
+
