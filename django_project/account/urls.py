1dacbcfc80fe19f15842b62211787a731f6f7b3d
Pineapplemocha24 <blessingisrael625@gmail.com>
2023-06-01 16:36:28 +0300

    ...

--- django_project/account/urls.py
+++ django_project/account/urls.py
@@ -11,6 +11,4 @@ urlpatterns = [
     path('recipient_dashboard/', views.recipient_dashboard, name='recipient_dashboard'),
     path('sender_dashboard/', views.sender_dashboard, name='sender_dashboard'),
     path('courier_dash/', views.courier_dashboard, name='courier_dash'),
-    path('notification_count/', views.get_notification_count, name='notification_count'),
-    path('completed_package/', views.completed_package, name='completed_package'),
 ]
--- django_project/account/views.py
+++ django_project/account/views.py
@@ -34,48 +34,8 @@ def sender_dashboard(request):
 def courier_dashboard(request):
     return render(request, 'courier_dash.html', {})
 
-def completed_package(request):
-    return render(request, 'completed_package.html', {})
-
 def register_package(request):
     return render(request, 'register_package.html', {})
-
-def get_notification_count(request):
-    # Get the current user
-    user = request.user
-
-    # Retrieve the count of unread notifications for the user
-    notification_count = Notification.objects.filter(user=user, is_read=False).count()
-
-    # Send notifications dynamically based on package status
-    if user.role == 'recipient':
-        # Get the packages where the recipient is the current user
-        packages = Package.objects.filter(recipient=user)
-
-        # Loop through the packages and check the status
-        for package in packages:
-            if package.status == 'completed' and not package.notification_sent:
-                # Create a notification message for completed packages
-                message = f"The package '{package.packageName}' has been delivered."
-
-                # Check if the user is online
-                if user.is_online:
-                    # Create a notification and associate it with the recipient user
-                    notification = Notification.objects.create(user=user, message=message)
-                else:
-                    # Create an offline notification and associate it with the recipient user
-                    offline_notification = OfflineNotification.objects.create(user=user, message=message)
-
-                # Set the package's notification_sent field to True to prevent duplicate notifications
-                package.notification_sent = True
-                package.save()
-
-    context = {
-        'notification_count': notification_count
-    }
-
-    return render(request, 'notification_count.html', context)
-
 
 def logout_user(request):
     logout(request)
@@ -134,20 +94,5 @@ def register_package(request):
     return render(request, 'sender_dashboard.html', {'form': form, 'packages': packages})
 
 
-def delivered_packages_api(request):
-    delivered_packages = Package.objects.filter(status='Completed')
-
-    # Create a list to hold the package data
-    package_list = []
-    for package in delivered_packages:
-        package_data = {
-            'number': package.pk,
-            'title': package.packageName,
-            'deliveringTo': package.recipientName,
-            'status': package.status,
-        }
-        package_list.append(package_data)
-
-    return JsonResponse(package_list, safe=False)
 
 
