c2783970335202016de3a39b3eff3305cc79197d
InTheYearOf39 <garynkuraija@gmail.com>
2023-06-20 17:04:38 +0300

    adding Warehouse tag to package

--- django_project/account/views/warehouse_views.py
+++ django_project/account/views/warehouse_views.py
@@ -95,10 +95,10 @@ def confirm_arrival(request, package_id):
         sender_message = f"Your package with ID {package.package_number} has arrived at the warehouse."
         send_mail('Package Arrival Notification', sender_message, 'garynkuraiji@gmail.com', [sender_email])
 
-        # Send email to warehouse
-        warehouse_email = 'warehouse@example.com'  # Replace with actual warehouse email
-        warehouse_message = f"A package with ID {package.package_number} has arrived at the warehouse."
-        send_mail('Package Arrival Notification', warehouse_message, 'sender@example.com', [warehouse_email])
+        # # Send email to warehouse
+        # warehouse_email = 'warehouse@example.com'  # Replace with actual warehouse email
+        # warehouse_message = f"A package with ID {package.package_number} has arrived at the warehouse."
+        # send_mail('Package Arrival Notification', warehouse_message, 'sender@example.com', [warehouse_email])
 
         messages.success(request, "Package arrival notified successfully.")
     else:
