2544570c6306326019017b4f674831cd1d5a4f0e
Muhu-Muza <schumannivan@gmail.com>
2023-09-14 20:33:18 +0300

    CHORES/clean up

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -98,17 +98,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'django_project.wsgi.application'
 
-ASGI_APPLICATION = "django_project.asgi.application"
-# Use Redis as the channel layer (for production)
-CHANNEL_LAYERS = {
-    "default": {
-        "BACKEND": "channels_redis.core.RedisChannelLayer",
-        "CONFIG": {
-            "hosts": [("127.0.0.1", 6379)],
-        },
-    },
-}
-
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
